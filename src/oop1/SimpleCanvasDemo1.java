f4bc1d00d4617c13209a3d518120049c0015d460
Phil Kirlin <kirlinp@rhodes.edu>
2022-09-13 09:59:44 -0500

    updates

--- src/oop1/SimpleCanvasDemo1.java
+++ src/oop1/SimpleCanvasDemo1.java
@@ -6,22 +6,22 @@ public class SimpleCanvasDemo1 {
     public static void main(String[] args) {
 
         // Declare a new SimpleCanvas variable called canvas.
-        SimpleCanvas canvas;
+        SimpleCanvas c;
 
         // Instantiate a new SimpleCanvas object by calling a constructor.
         // This creates a new instance of the SimpleCanvas class, and assigns
         // that instance to the canvas variable.
-        canvas = new SimpleCanvas(400, 200, "My Canvas");
+        c = new SimpleCanvas(400, 200, "My Canvas");
 
         // Note that we could have done both lines together:
         // SimpleCanvas canvas = new SimpleCanvas(400, 200, "My Canvas");
 
         // Invoke some instance methods that operate on the canvas.
-        canvas.setPenColor(Color.BLUE);
-        canvas.drawFilledRectangle(100, 50, 50, 100);
-        canvas.setPenColor(Color.RED);
-        canvas.setLineThickness(5);
-        canvas.drawCircle(300, 100, 50);
-        canvas.show();
+        c.setPenColor(Color.BLUE);
+        c.drawFilledRectangle(100, 50, 50, 100);
+        c.setPenColor(Color.RED);
+        c.setLineThickness(5);
+        c.drawCircle(300, 100, 50);
+        c.show();
     }
 }
