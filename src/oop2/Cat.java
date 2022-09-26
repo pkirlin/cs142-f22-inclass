57805dbe040df5898400351a383ac9c2bcdde284
Phil Kirlin <kirlinp@rhodes.edu>
2022-09-26 09:26:54 -0500

    updates

--- src/oop2/Cat.java
+++ src/oop2/Cat.java
@@ -1,10 +1,27 @@
 package oop2;
 
 public class Cat {
-    String name;
-    int age;
-    double weight;
-    int energy;
+    private String name;
+    private int age;
+    private double weight;
+    private int energy;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        if (name.equals("")) {
+            System.out.println("You bozo, you can't set the cat's name to nothing");
+        }
+        else {
+            name = newName;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
 
     void meow() {
         System.out.println(name + " says meow!!");
--- src/oop2/CatDemo.java
+++ src/oop2/CatDemo.java
@@ -6,15 +6,15 @@ public class CatDemo {
         Cat tank = new Cat();
         pumpkin.age = 9;
         tank.age = 14;
-        pumpkin.name = "Pumpkin";
-        tank.name = "Tank the Wise";
-        System.out.println(tank.name + " is " + tank.age + " years old");
-        System.out.println(pumpkin.name + " is " + pumpkin.age + " years old");
+        pumpkin.setName("Pumpkin");
+        tank.setName("Tank the Wise");
+        System.out.println(tank.getName() + " is " + tank.getAge() + " years old");
+        System.out.println(pumpkin.getName() + " is " + pumpkin.getAge() + " years old");
 
         Cat anotherCat = pumpkin;
-        System.out.println(anotherCat.name + " is " + anotherCat.age + " years old");
+        System.out.println(anotherCat.getName() + " is " + anotherCat.getAge() + " years old");
         anotherCat.age++;
-        System.out.println(pumpkin.name + " is " + pumpkin.age + " years old");
+        System.out.println(pumpkin.getName() + " is " + pumpkin.getAge() + " years old");
         anotherCat = tank;
         anotherCat.age++;
         System.out.println(tank.name + " is " + tank.age + " years old");
