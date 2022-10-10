d3f2872acbe23190367e54fd0350f890ca0c2f46
Phil Kirlin <kirlinp@rhodes.edu>
2022-10-10 10:54:23 -0500

    updates

--- src/fraction/startOct10/Fraction.java
+++ src/fraction/startOct10/Fraction.java
@@ -13,6 +13,29 @@ public class Fraction {
         int divisor = ExtraMath.gcd(n, d);
         numerator = n/divisor;
         denominator = d/divisor;
+
+        // check to make sure negative fractions
+        // are formed correctly.
+        if (denominator < 0) {
+            denominator = -denominator;
+            numerator = -numerator;
+        }
+    }
+
+    public Fraction negate() {
+        // return a new fraction that is the
+        // negation of this fraction.
+        int newNumerator = -numerator;
+        int newDenominator = denominator;
+        Fraction newFrac = new Fraction(newNumerator, newDenominator);
+        return newFrac;
+    }
+
+    public Fraction add(Fraction otherFraction) {
+        int newNumerator = numerator * otherFraction.denominator + denominator * otherFraction.numerator;
+        int newDenominator = denominator * otherFraction.denominator;
+        Fraction newFrac = new Fraction(newNumerator, newDenominator);
+        return newFrac;
     }
 
     public String toString() {
--- src/fraction/startOct10/FractionDemo.java
+++ src/fraction/startOct10/FractionDemo.java
@@ -2,9 +2,15 @@ package fraction.startOct10;
 
 public class FractionDemo {
     public static void main(String[] args) {
-        Fraction frac = new Fraction(2, 3);
-        Fraction frac2 = new Fraction(6, 8);
+        Fraction frac = new Fraction(1, 2);
+        Fraction frac2 = new Fraction(1, 2);
+        Fraction frac3 = new Fraction(2, 3);
         System.out.println(frac);
         System.out.println(frac2);
+        System.out.println(frac.negate());
+        System.out.println(frac2.negate());
+        System.out.println(frac.add(frac2));
+
+
     }
 }
