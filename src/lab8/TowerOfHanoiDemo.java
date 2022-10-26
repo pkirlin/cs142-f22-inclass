e82077bdc82f3be47daf068140e33403fb7c2b1b
Phil Kirlin <kirlinp@rhodes.edu>
2022-10-26 08:46:07 -0500

    updates

---
+++ src/lab8/TowerOfHanoiDemo.java
@@ -0,0 +1,11 @@
+package lab8;
+
+public class TowerOfHanoiDemo {
+    public static void main(String[] args)
+    {
+        // You can modify the number of disks below,
+        // as well as the pause time between moves (milliseconds).
+        TowerOfHanoi game = new TowerOfHanoi(3, 1000);
+        game.run();
+    }
+}
