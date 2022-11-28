fa904633cf06b031dd154cafdedad47d70905a94
Phil Kirlin <kirlinp@rhodes.edu>
2022-11-28 11:24:16 -0600

    updates

--- src/interfaces1/DrawingDemo.java
+++ src/interfaces1/DrawingDemo.java
@@ -14,13 +14,25 @@ public class DrawingDemo {
 
         // Write code here to make an arraylist and add the
         // objects we want to draw into the arraylist.
+        ArrayList<Drawable> thingsToDraw = new ArrayList<Drawable>();
+        thingsToDraw.add(circle);
+        thingsToDraw.add(square);
+        thingsToDraw.add(fido);
+        thingsToDraw.add(lassie);
 
         // Call the drawAllObjects function.
+        drawAllObjects(thingsToDraw);
     }
 
-    /*public static void drawAllObjects(ArrayList<???> list)
+    public static void drawAllObjects(ArrayList<Drawable> list)
     {
         SimpleCanvas canvas = new SimpleCanvas(500, 500);
         canvas.show();
-    }*/
+
+        // write code here to draw all the objects
+        for (Drawable d : list) {
+            d.drawOn(canvas, 100, 200);
+        }
+        canvas.update();
+    }
 }
